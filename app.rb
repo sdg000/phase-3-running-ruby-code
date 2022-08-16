f5d6ab01b29ce77c7b36b056b67f6e912b16d4af
Seth David Gyimah <esec26@gmail.com>
2022-08-16 22:19:52 +0000

    first ruby code along

---
+++ app.rb
@@ -0,0 +1,20 @@
+# #this is my first ruby play
+
+# #OUTPUT / PRINT IN RUBY
+# #puts - prints on new line
+# puts "Hello world!"
+# puts "Hello world!"
+
+# #print - prints on a new same line
+
+# #p - printing non string data types
+
+# #pp  - pretty-printing : for printing nested / complex data types
+# pp [{id: 1, hello: "World"}, {id: 2, hello: "Ruby"}, {id: 3, hello: "Moon"}]
+
+# PASSING TESTS
+puts "Hello World!"
+
+p [1, 2, 3]
+
+print "Pass this test, please."
